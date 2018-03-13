5434f3b2881fc551be3c8e9bcf4cdcef1ee27a74
w@phpuse.cn <wja@528624>
2018-03-13 16:51:32 +0800

    更新token

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -21,6 +21,7 @@ return array(
     'DB_PART' => '20971520',   //该值用于限制压缩后的分卷最大长度。单位：B；建议设置20M
     'DB_COMPRESS' => '1',      //压缩备份文件需要PHP环境支持gzopen,gzwrite函数        0:不压缩 1:启用压缩
     'DB_LEVEL' => '9',         //压缩级别   1:普通   4:一般   9:最高
+	'URL_MODEL' => '2',
     //扩展配置文件
     'LOAD_EXT_CONFIG' => 'db',
     //用户等级设置
